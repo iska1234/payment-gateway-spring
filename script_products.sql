cde05627ca82a39b22d02f4f557c4f1afe24df54
iska1234 <119825666+iska1234@users.noreply.github.com>
2023-08-11 10:41:42 -0500

    Scriptsql

---
+++ script_products.sql
@@ -0,0 +1,25 @@
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodacer','Monitor Acer HA220Q 21.5 FULL HD LED IPS 75HZ',549,'/images/Acerproducto1.png');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodacer','Monitor Acer Nitro VG240Y 23.8 FULL HD LED IPS 144hz"',910,'/images/Acerproducto2.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodacer','Monitor Acer Nitro XZ270 27 FULL HD LED IPS 240hz"',1490,'/images/Acerproducto3.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodacer','Monitor Acer Nitro VG280K 28 4K LED IPS 60hz"',1449,'/images/Acerproducto4.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodasus','TUF Gaming A15FA5061C 15.6 FHD 512GB SSD 8GB "',3899,'/images/Asusprod1.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodasus','Vivobook 13 OLED T3300KA 13.3 256GB SSD 8GB RAM"',2799,'/images/Asusprod2.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodasus','ASUS ROG STRIX 15.6 AMD RYZEN 7 512GB SSD GRAY',5359,'/images/Asusprod3.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodasus','VIVOBOOK X413 14 FHD 128GB SSD 4GB DDR4',2159,'/images/Asusprod4.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodmsi','MSI GF63 15.6 FHD 256GB SSD 8GB RAM GTX 1650"',2899,'/images/Msiprod1.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodmsi','MONITOR MSI DE 27 MSI OPTIX G273QF',1650,'/images/Msiprod2.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodmsi','MOUSE MSI CLUTCH GM41 LIGHTWEIGHT',250,'/images/Msiprod3.jpg');
+insert into ventasweb.producto(fecha_creacion,marca,nombre,precio,ruta)value
+('20220813','caja-prod prodmsi','MSI KATANA GF66 15.6 512GB SSD RTX 3050"',5544,'/images/Msiprod4.jpg');
+
